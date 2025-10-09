4ae5e82bc78c19cd942f3c4d8081866e441b4526
Ayush Singh <ayusingh693@gmail.com>
2025-10-10 00:53:44 +0530

    setup created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ayush Singh",
+    author_email="ayusingh693@gmail.com",
+    packages=find_packages()
+)
